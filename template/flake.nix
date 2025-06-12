6aeebfd1acc3d18997e2c7c4d6a25c0d421a9024
Filip Pękala <272985@student.pwr.edu.pl>
2025-06-12 19:35:06 +0200

    Prepare real template

---
+++ template/flake.nix
@@ -0,0 +1,17 @@
+{
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/unstable";
+
+		nixosBlankSystem.url = "github:fpekal-nixos/nixos-template";
+		nixosBlankSystem.inputs.nixpkgs.follows = "nixpkgs";
+	};
+
+	outputs =
+		{ self, nixosBlankSystem }:
+		{
+			systems.minimal = nixosBlankSystem.systems.blank.extend { modules = [ ./modules ./overlays ./packages ./users ./hosts ./shells ./options ]; };
+
+			nixosConfigurations.default = self.systems.minimal.mksystem { system = "x86_64-linux"; };
+		};
+}
+

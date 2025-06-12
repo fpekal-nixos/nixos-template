58b8256396e2e468949b7312a6ff13b29d504fa7
Filip Pękala <272985@student.pwr.edu.pl>
2025-06-12 19:28:50 +0200

    Add the framework to the main flake.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,17 @@
 {
   description = "A modular NixOS configuration flake template.";
 
-  outputs = { self }: {
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs";
+		framework.url = "./framework";
+		framework.inputs.nixpkgs.follows = "nixpkgs";
+	};
+
+  outputs = { framework, ... }: {
 		templates = {
 			default = {
 				path = ./template;
 			};
 		};
-  };
+  } // framework;
 }

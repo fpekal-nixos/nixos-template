10a6344f266c63a47b6266d43bcd2b856528b998
Filip Pękala <272985@student.pwr.edu.pl>
2025-06-12 19:53:21 +0200

    Fix template

--- template/flake.nix
+++ template/flake.nix
@@ -1,13 +1,13 @@
 {
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/unstable";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
 		nixosBlankSystem.url = "github:fpekal-nixos/nixos-template";
 		nixosBlankSystem.inputs.nixpkgs.follows = "nixpkgs";
 	};
 
 	outputs =
-		{ self, nixosBlankSystem }:
+		{ self, nixosBlankSystem, ... }:
 		{
 			systems.minimal = nixosBlankSystem.systems.blank.extend { modules = [ ./modules ./overlays ./packages ./users ./hosts ./shells ./options ]; };
 

7afc4ffddba1aa62e9ba15c2ccab2741e68788a6
Filip Pękala <272985@student.pwr.edu.pl>
2025-06-12 16:15:01 +0200

    Extend and mksystem functions

--- template/flake.nix
+++ template/flake.nix
@@ -5,7 +5,33 @@
 
 	outputs =
 	{ self, nixpkgs, }:
+		let
+			mksystem_func =
+				{ _m, _s }:
+				{ modules ? [], specialArgs ? {}, system }:
+				nixpkgs.lib.nixosSystem {
+					inherit system;
+
+					modules = modules ++ _m;
+					specialArgs = specialArgs // _s;
+				};
+
+			extend_func =
+				{ _m, _s }:
+				{
+					extend = { modules ? [], specialArgs ? {} }: extend_func { _m = modules ++ _m; _s = specialArgs // _s; };
+					mksystem = mksystem_func { inherit _m _s; };
+				};
+
+			blank_system = extend_func { _m = []; _s = {}; };
+
+		in
 		{
-			
+			systems.blank = blank_system;
+			systems.minimal = self.systems.blank.extend { modules = [ ./modules ./overlays ./packages ./users ./hosts ./shells ]; };
+
+			nixosConfigurations = {
+				default = self.systems.minimal.mksystem { system = "x86_64-linux"; };
+			};
 		};
 }
---
+++ template/lib/mksystem.nix
@@ -0,0 +1,5 @@
+{ nixpkgs }:
+{ system, modules, specialArgs }:
+{
+	
+}
